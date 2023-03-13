842f7622435560bcdeaf2f783eb4926faa1b3b15
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-03-13 21:15:59 +0100

    exercise completed...now work on bonus part ;)

--- main.js
+++ main.js
@@ -112,3 +112,49 @@ const icons = [
 		color: 'blue'
 	}
 ];
+
+const containerCardsDom = document.getElementById("container");
+const inputDom = document.getElementById("yourIcon");
+let type = [];
+
+icons.forEach((oggetto) => {
+    cardGenerator(containerCardsDom,oggetto);
+   if (!type.includes(oggetto.type)){
+      type.push(oggetto.type);
+   }
+   
+});
+
+for (i = 0; i <= type.length; i++) {
+    let option;
+    if (i === 0) {
+      option = '<option value="all">All</option>';
+    } else {
+      option = `<option value="${type[i-1]}">${type[i-1]}</option>`;
+    }
+    inputDom.innerHTML += option;
+};
+  
+
+inputDom.addEventListener("change", () => {
+    containerCardsDom.innerHTML="";
+    icons.map((card) => {
+       if (inputDom.value === card.type || inputDom.value === "all") {
+          return cardGenerator(containerCardsDom, card);
+       }
+    });
+});
+
+
+function cardGenerator(position, iconCard) {
+
+    let card = `  <div class="cardContainer">
+                      <div class="card">
+                         <h2 class="card-title"><i class="fa-solid ${iconCard.prefix}${iconCard.name}" style="color: ${iconCard.color};"></i></h2>
+                         <p class="card-text">${iconCard.name}</p>
+                      </div>
+                   </div>` 
+ 
+    position.innerHTML += card;
+};
+ 
